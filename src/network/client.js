a49bde69e9e54a5826efceccf305ab863017cdb9
Keshav Gupta <keshav.pg@gmail.com>
2024-02-04 20:21:06 +0100

    Sucessful client communication using peerSub

--- src/network/client.js
+++ src/network/client.js
@@ -1,16 +1,37 @@
 const { from, interval, timer, of } = require('rxjs');
 const { switchMap, catchError, retryWhen, delayWhen, tap, finalize } = require('rxjs/operators');
 const Link = require('grenache-nodejs-link');
-const { PeerRPCClient } = require('grenache-nodejs-ws');
+const { PeerRPCClient, PeerSub } = require('grenache-nodejs-ws');
 const Order = require('../models/Order');
 const moment = require('moment');
 const crypto = require('crypto');
 
 // Initialize Grenache Link and PeerRPCClient
-const link = new Link({ grape: 'http://127.0.0.1:40001' });
+const link = new Link({ grape: 'http://192.168.1.29:40001' });
 link.start();
 const peer = new PeerRPCClient(link, {});
 peer.init();
+
+// Initialize PeerSub for listening to broadcasts
+const peerSub = new PeerSub(link, {});
+peerSub.init();
+
+// Subscribe to the desired topic
+peerSub.sub('order_updates', { timeout: 10000 });
+
+peerSub.on('connected', () => {
+  console.log('Subscription connected');
+});
+
+peerSub.on('disconnected', () => {
+  console.log('Subscription disconnected');
+});
+
+// Handle incoming messages
+peerSub.on('message', (msg) => {
+  console.log('Received message:', msg);
+  // Here you can add logic to process the message
+});
 
 // Generate a unique identifier for the peer
 const peerId = crypto.createHash('sha256').update(`${moment().toISOString()}${Math.random()}`).digest('hex').substr(0, 6);
@@ -32,7 +53,7 @@ const createOrderObservable = (actionType) => {
   }
 
   return from(new Promise((resolve, reject) => {
-    peer.request('rpc_test', payload, { timeout: 10000 }, (err, data) => {
+    peer.request('order_service', payload, { timeout: 10000 }, (err, data) => {
       if (err) {
         reject(err);
       } else {
@@ -49,7 +70,7 @@ const createOrderObservable = (actionType) => {
 };
 
 // Interval observable for sending orders
-const orderRequestInterval$ = interval(2000).pipe(
+const orderRequestInterval$ = interval(1000).pipe(
   switchMap(() => createOrderObservable(Math.random() > 0.5 ? 'ADD_ORDER' : 'DELETE_ORDER')),
   retryWhen(errors =>
     errors.pipe(
@@ -66,3 +87,5 @@ orderRequestInterval$.subscribe({
   error: err => console.error('Observable encountered an error:', err),
   complete: () => console.log('Observable completed')
 });
+
+
