2d19d3a9051315b6480c5a2aa43baa43db1f4b28
Keshav Gupta <keshav.pg@gmail.com>
2024-02-04 17:21:17 +0100

    PeerId added to order UUID

--- src/network/client.js
+++ src/network/client.js
@@ -2,6 +2,7 @@ const Link = require('grenache-nodejs-link');
 const { PeerRPCClient } = require('grenache-nodejs-ws');
 const Order = require('../models/order');
 const moment = require('moment');
+const crypto = require('crypto');
 
 const link = new Link({
   grape: 'http://127.0.0.1:30001'
@@ -11,12 +12,25 @@ link.start();
 const peer = new PeerRPCClient(link, {});
 peer.init();
 
+/*
+* Generate a Unique Identifier for Each Peer
+* When the peer starts, generate a unique identifier
+* This could be based on a hash of the current timestamp,
+* a random number, or other system-specific information
+* that remains constant for the lifetime of the peer.
+
+* Incorporate this Identifier into the Order ID: Include this unique identifier in the order ID
+* along with the timestamp and a sequence number.
+*/ 
 let sequenceNum = 0;
+
+// Generate a unique hash for this peer instance
+const peerId = crypto.createHash('sha256').update(moment().toISOString() + Math.random().toString()).digest('hex').substr(0, 6); // Short hash of 6 characters
 
 function generateOrderId() {
   const timestamp = moment().format('YYYYMMDDHHmmssSSS');
   sequenceNum += 1;
-  return `order_${timestamp}_${sequenceNum}`;
+  return `order_${peerId}_${timestamp}_${sequenceNum}`;
 }
 
 function sendOrder() {
